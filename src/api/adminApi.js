ef00afa135701b074ca6bf245db8911298ae4d18
Hoang Anh Quan <hoangquan29112003@gmail.com>
2026-03-31 15:18:04 +0700

    update transaction filter

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,13 +1,14 @@
 import axiosClient from './axiosClient'
 
+
 export const getUsers = () => {
   return axiosClient.get('/admin/users')
 }
-
-export const getAllTransactions = (page = 0) => {
-  return axiosClient.get(`/admin/transactions?page=${page}&size=10`)
-}
-
+export const getAllTransactions = (params) => {
+  return axiosClient.get(`/admin/transactions`, {
+    params,
+  });
+};
 export const getSuspicious = (page = 0, size = 10) => {
   return axiosClient.get(`/admin/transactions/suspicious?page=${page}&size=${size}`)
 }
--- src/screens/admin/TransactionListScreen.js
+++ src/screens/admin/TransactionListScreen.js
@@ -4,34 +4,60 @@ import { getAllTransactions } from "../../api/adminApi";
 import { StyleSheet, Image } from "react-native";
 import { COLORS, SIZES, SHADOW } from "../../theme/theme";
 import logo from "../../assets/logo.png";
+import { TextInput, TouchableOpacity } from "react-native";
+import axios from "axios";
+import DateTimePicker from "@react-native-community/datetimepicker";
 export default function TransactionListScreen() {
   const [transactions, setTransactions] = useState([]);
   const [page, setPage] = useState(0);
-
+const [email, setEmail] = useState("");
+const [type, setType] = useState("");
+const [status, setStatus] = useState("");
+const [from, setFrom] = useState("");
+const [to, setTo] = useState("");
+const [showFromPicker, setShowFromPicker] = useState(false);
+const [showToPicker, setShowToPicker] = useState(false);
+const [reload, setReload] = useState(false);
 const fetchData = async () => {
   try {
     console.log("FETCH PAGE:", page);
 
-    const res = await getAllTransactions(page);
+    let params = {
+      page,
+      size: 10,
+    };
+
+    if (type) params.type = type;
+    if (status) params.status = status;
+    if (from) params.from = from.slice(0, 19);
+if (to) params.to = to.slice(0, 19);
+
+if (email) params.email = email;
+
+    const res = await getAllTransactions(params);
 
     const newData = res.data.content;
 
     console.log("NEW DATA:", newData.length);
 
-    if (newData.length === 0) return; // ❌ không set rỗng
-
-    setTransactions(prev => [...prev, ...newData]);
+    if (page === 0) {
+      setTransactions(newData); 
+    } else {
+      setTransactions(prev => [...prev, ...newData]);
+    }
 
   } catch (e) {
     console.log("FETCH ERROR:", e);
   }
 };
-
-  useEffect(() => {
-    console.log("USE EFFECT RUN");
-    fetchData();
-    console.log("STATE transactions:", transactions);
-  }, [page]);
+const applyFilter = () => {
+  setTransactions([]);
+  setPage(0);
+  setReload(prev => !prev); 
+};
+useEffect(() => {
+  fetchData();
+}, [page, reload]);
 
 return (
   <View style={styles.container}>
@@ -42,7 +68,134 @@ return (
       <Text style={styles.title}>All Transactions</Text>
       <Text style={styles.subtitle}>Monitor system activity</Text>
     </View>
+<View style={styles.filterBox}>
 
+  {/* EMAIL */}
+  <TextInput
+    placeholder="Search email"
+    value={email}
+    onChangeText={setEmail}
+    style={styles.input}
+  />
+
+  {/* TYPE */}
+  <View style={styles.row}>
+    <TouchableOpacity
+  onPress={() => setType("TRANSFER")}
+  style={[
+    styles.input,
+    type === "TRANSFER" && { backgroundColor: "#4CAF50" }
+  ]}
+>
+  <Text style={{ color: type === "TRANSFER" ? "white" : "black" }}>
+    TRANSFER
+  </Text>
+</TouchableOpacity>
+
+    <TouchableOpacity
+  onPress={() => setType("DEPOSIT")}
+  style={[
+    styles.input,
+    type === "DEPOSIT" && { backgroundColor: "#4CAF50" }
+  ]}
+>
+  <Text style={{ color: type === "DEPOSIT" ? "white" : "black" }}>
+    DEPOSIT
+  </Text>
+</TouchableOpacity>
+
+    <TouchableOpacity
+  onPress={() => setType("")}
+  style={[
+    styles.input,
+    type === "" && { backgroundColor: "#4CAF50" }
+  ]}
+>
+  <Text style={{ color: type === "" ? "white" : "black" }}>
+    ALL
+  </Text>
+</TouchableOpacity>
+  </View>
+
+  {/* STATUS */}
+  <View style={styles.row}>
+    <TouchableOpacity
+    onPress={() => setStatus("SUCCESS")}
+    style={[
+      styles.input,
+      status === "SUCCESS" && { backgroundColor: "#4CAF50" }
+    ]}
+  >
+    <Text style={{ color: status === "SUCCESS" ? "white" : "black" }}>
+      SUCCESS
+    </Text>
+  </TouchableOpacity>
+
+    <TouchableOpacity
+    onPress={() => setStatus("FAILED")}
+    style={[
+      styles.input,
+      status === "FAILED" && { backgroundColor: "#4CAF50" }
+    ]}
+  >
+    <Text style={{ color: status === "FAILED" ? "white" : "black" }}>
+      FAILED
+    </Text>
+  </TouchableOpacity>
+
+    <TouchableOpacity
+    onPress={() => setStatus("")}
+    style={[
+      styles.input,
+      status === "" && { backgroundColor: "#4CAF50" }
+    ]}
+  >
+    <Text style={{ color: status === "" ? "white" : "black" }}>
+      ALL
+    </Text>
+  </TouchableOpacity>
+  </View>
+
+<TouchableOpacity onPress={() => setShowFromPicker(true)}>
+  <Text style={styles.input}>
+    {from ? new Date(from).toLocaleDateString() : "Select From Date"}
+  </Text>
+</TouchableOpacity>
+
+{showFromPicker && (
+  <DateTimePicker
+    value={new Date()}
+    mode="date"
+    onChange={(e, date) => {
+      setShowFromPicker(false);
+      if (date) setFrom(date.toISOString());
+    }}
+  />
+)}
+
+<TouchableOpacity onPress={() => setShowToPicker(true)}>
+  <Text style={styles.input}>
+    {to ? new Date(to).toLocaleDateString() : "Select To Date"}
+  </Text>
+</TouchableOpacity>
+
+{showToPicker && (
+  <DateTimePicker
+    value={new Date()}
+    mode="date"
+    onChange={(e, date) => {
+      setShowToPicker(false);
+      if (date) setTo(date.toISOString());
+    }}
+  />
+)}
+
+  {/* APPLY */}
+  <TouchableOpacity style={styles.button} onPress={applyFilter}>
+    <Text>Apply Filter</Text>
+  </TouchableOpacity>
+
+</View>
     {/* LIST */}
     <FlatList
       contentContainerStyle={{ padding: SIZES.padding }}
@@ -179,4 +332,35 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: COLORS.secondary,
   },
+  input: {
+  borderWidth: 1,
+  borderColor: "#ccc",
+  padding: 10,
+  borderRadius: 8,
+  marginVertical: 6,
+  backgroundColor: "white",
+},
+
+row: {
+  flexDirection: "row",
+  justifyContent: "space-between",
+  marginVertical: 6,
+},
+
+filterBox: {
+  backgroundColor: "white",
+  margin: 10,
+  padding: 10,
+  borderRadius: 10,
+  borderWidth: 1,
+  borderColor: "#ddd",
+},
+
+button: {
+  backgroundColor: "#4CAF50",
+  padding: 12,
+  borderRadius: 8,
+  alignItems: "center",
+  marginTop: 10,
+},
 });
